4dc20b5b48834a219e45b9eef6450e6fb928e5bf
Andres <brinezlopez08@gmail.com>
2022-12-10 23:06:20 -0500

    Cambios

--- static/js/api.js
+++ static/js/api.js
@@ -1,10 +1,53 @@
 import { mostrarHTML } from "./html.js"
 
 export async  function conection(city){
-    const API = 'http://api.weatherstack.com/current?access_key=c42df8df799b1f4740b2055d2396b44a&query='
-    const url = API + city
-    const res = await  fetch(url) // obtiene la respuesta
-    const data = await res.json() // convierte la respuesta en json
+    // const API = 'http://api.weatherstack.com/current?access_key=c42df8df799b1f4740b2055d2396b44a&query='
+    // const url = API + city
+    // const res = await  fetch(url) // obtiene la respuesta
+    // const data = await res.json() // convierte la respuesta en json
+    // console.log(data)
+
+    let data={
+        "request": {
+          "type": "City",
+          "query": "Cajamarca, Peru",
+          "language": "en",
+          "unit": "m"
+        },
+        "location": {
+          "name": "Cajamarca",
+          "country": "Peru",
+          "region": "Cajamarca",
+          "lat": "-7.167",
+          "lon": "-78.517",
+          "timezone_id": "America/Lima",
+          "localtime": "2022-12-10 20:54",
+          "localtime_epoch": 1670705640,
+          "utc_offset": "-5.0"
+        },
+        "current": {
+          "observation_time": "01:54 AM",
+          "temperature": 7,
+          "weather_code": 353,
+          "weather_icons": [
+            "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0025_light_rain_showers_night.png"
+          ],
+          "weather_descriptions": [
+            "Light rain shower"
+          ],
+          "wind_speed": 5,
+          "wind_degree": 115,
+          "wind_dir": "ESE",
+          "pressure": 1017,
+          "precip": 0.3,
+          "humidity": 94,
+          "cloudcover": 60,
+          "feelslike": 6,
+          "uv_index": 1,
+          "visibility": 10,
+          "is_day": "no"
+        }
+    }
 
     return  data
 
