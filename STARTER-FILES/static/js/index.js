14896654bf286c93d9c1c13f7a6d7131b2d82935
Andres <brinezlopez08@gmail.com>
2022-12-10 21:10:40 -0500

    Subir archivos

---
+++ STARTER-FILES/static/js/index.js
@@ -0,0 +1,7 @@
+import { conection } from './api.js'
+
+
+
+
+
+
---
+++ STARTER-FILES/static/objeto.js
@@ -0,0 +1,41 @@
+datos={
+    "request": {
+      "type": "City",
+      "query": "Cajamarca, Peru",
+      "language": "en",
+      "unit": "m"
+    },
+    "location": {
+      "name": "Cajamarca",
+      "country": "Peru",
+      "region": "Cajamarca",
+      "lat": "-7.167",
+      "lon": "-78.517",
+      "timezone_id": "America/Lima",
+      "localtime": "2022-12-10 20:54",
+      "localtime_epoch": 1670705640,
+      "utc_offset": "-5.0"
+    },
+    "current": {
+      "observation_time": "01:54 AM",
+      "temperature": 7,
+      "weather_code": 353,
+      "weather_icons": [
+        "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0025_light_rain_showers_night.png"
+      ],
+      "weather_descriptions": [
+        "Light rain shower"
+      ],
+      "wind_speed": 5,
+      "wind_degree": 115,
+      "wind_dir": "ESE",
+      "pressure": 1017,
+      "precip": 0.3,
+      "humidity": 94,
+      "cloudcover": 60,
+      "feelslike": 6,
+      "uv_index": 1,
+      "visibility": 10,
+      "is_day": "no"
+    }
+}
